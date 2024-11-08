294d12659252ce9a87ada8cf4ba98fb5332aa38c
shindaeho <sdh077@naver.com>
2024-11-08 14:42:32 +0900

    deepzoom

--- app/(multi)/deepzoom/page.tsx
+++ app/(multi)/deepzoom/page.tsx
@@ -7,9 +7,11 @@ const Deepzoom = dynamic(() => import("./deepzoom"), {
 export default function page() {
 
   return (
-    <>
-      <ImageUpload />
-      <Deepzoom />
-    </>
+    <div className='grid grid-cols-1 md:grid-cols-3 w-full h-full container gap-8'>
+      {/* <ImageUpload /> */}
+      <Deepzoom deepzoom={'두오모'} className='relative md:col-span-2' />
+      <Deepzoom deepzoom={'돈까스'} />
+      <Deepzoom deepzoom={'nebula'} className='md:col-span-3' />
+    </div>
   )
 }
