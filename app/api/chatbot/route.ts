b28e62cae827e299be3a7687b4ae9c28a7dafada
shindaeho <sdh077@naver.com>
2024-12-09 21:01:17 +0900

    간소화

--- app/api/chatbot/route.ts
+++ app/api/chatbot/route.ts
@@ -3,15 +3,6 @@ import { createClient } from "@/lib/supabase/server"
 export async function POST(request: Request) {
   const body = await request.json()
   const supabase = await createClient()
-  const { data: action, error: error1 } = await supabase
-    .from('chatbot_action')
-    .upsert({ ...body.action, bot_id: body.bot.id })
-    .select()
-
-  const { data: intent, error: error2 } = await supabase
-    .from('chatbot_intent')
-    .upsert({ id: body.intent.id, name: body.intent.name, bot_id: body.bot.id })
-    .select()
 
   const { data: user, error } = await supabase
     .from('chatbot_request')
@@ -19,12 +10,12 @@ export async function POST(request: Request) {
       user_id: body.userRequest.user.id,
       utterance: body.userRequest.utterance,
       params: body.userRequest.params,
-      intent_id: body.intent.id,
-      action_id: body.action.id,
+      intent: body.intent,
+      action: body.action,
       bot_id: body.bot.id
     })
     .select()
-  console.log(error, error1, error2)
+  console.log(error)
 
   return Response.json({ user })
 }
